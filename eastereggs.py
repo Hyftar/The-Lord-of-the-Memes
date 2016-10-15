9c90acb1de20247d5ca2902c4f6d7ef6b743038b
chrisbio <chris123666321@gmail.com>
2016-10-14 21:49:27 -0400

    Easter egg modified

--- eastereggs.py
+++ eastereggs.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 class EasterEggs():
     @staticmethod
     def NameEggs(player):
@@ -17,5 +20,18 @@ class EasterEggs():
             for stat in player.stats:
                 stat[1] = 0
             print("Learn to python, pleb.")
+        elif player.name == "Hillary Clinton" or player.name == "Richard Nixon":
+            for stat in player.stats:
+                stat[1] = 0
+            print("It doesn't pay to be a crook!")
+        elif player.name == "PC master race":
+            for stat in player.stats:
+                stat[1] = 10
+            textlist = ["Fedora!", "420!", "Doritos!", "Mountain Dew!",
+                        "Anonymous!", "git gewd", "trolololololo", "wow!",
+                        "newfag!!", "consolers are plebians"]
+            for text in range(20):
+                n = int(numpy.random.randint(0, 10, 1))
+                print(textlist[n])
         else:
             return True
