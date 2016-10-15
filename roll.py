21ce844629ffa5e730794030eb262b785f593f12
Hyftar <simon_landry@ymail.com>
2016-10-14 20:25:08 -0400

    Clean roll.py

--- roll.py
+++ roll.py
@@ -3,7 +3,6 @@ import numpy
 
 class Roll(object):
     def StandardRoll(player, value):
-        
         weight = 4
         deviation = 1
         roll = round(numpy.random.normal(weight, deviation), 0)
@@ -11,4 +10,3 @@ class Roll(object):
             roll = 6
         elif roll < 0:
             roll = 0
-        
